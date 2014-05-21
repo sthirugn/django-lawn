15588ca39714482a0c72d8b37e03b7d32856eea9
sthirugn <sthirugn@redhat.com>
2014-05-20 22:54:47 -0400

    new css style added

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('home/home.html')
+
+def lawn(request):
+    return render_to_response('newhome/index.html')
