d67a66330d801902e692f87a166fe99288a5ba1e
sthirugn <sthirugn@redhat.com>
2014-06-03 00:07:04 -0400

    Added static pages for different features

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -9,3 +9,27 @@ def lawn(request):
 
 def services(request):
     return render_to_response('newhome/services.html')
+
+def aboutus(request):
+    return render_to_response('newhome/aboutus.html')
+
+def contactus(request):
+    return render_to_response('newhome/contactus.html')
+
+def lawnreadmore(request):
+    return render_to_response('newhome/lawnreadmore.html')
+
+def satisfactionreadmore(request):
+    return render_to_response('newhome/satisfactionreadmore.html')
+
+def testimonialsreadmore(request):
+    return render_to_response('newhome/testimonialsreadmore.html')
+
+def mowing(request):
+    return render_to_response('newhome/mowing.html')
+
+def shrubs(request):
+    return render_to_response('newhome/shrubs.html')
+
+def mulch(request):
+    return render_to_response('newhome/mulch.html')
